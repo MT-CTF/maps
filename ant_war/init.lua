6f3f780e15ea3642a6df2aba4acc298697bfff57
LoneWolfHT <lonewolf04361@gmail.com>
2023-08-15 10:14:58 -0700

    Rework init.lua

--- ant_war/init.lua
+++ ant_war/init.lua
@@ -1,61 +1,50 @@
-ctf_api.register_on_new_match(function()
-    local map = ctf_modebase.map_catalog.maps[ctf_modebase.map_catalog.current_map].name
-    if map == "Ant War" then
-		local closed = true
-		local offset = ctf_modebase.map_catalog.maps[ctf_modebase.map_catalog.current_map].offset
-		local barriers = {	{x=offset.x+82, y=offset.y+13, z=offset.z+80},
-							{x=offset.x+81, y=offset.y+13, z=offset.z+81},
-							{x=offset.x+80, y=offset.y+13, z=offset.z+82},
-							{x=offset.x+82, y=offset.y+14, z=offset.z+80},
-							{x=offset.x+81, y=offset.y+14, z=offset.z+81},
-							{x=offset.x+80, y=offset.y+14, z=offset.z+82},
-							{x=offset.x+81, y=offset.y+15, z=offset.z+81},
+local closed = true
 
-							{x=offset.x+80, y=offset.y+19, z=offset.z+80},
-							{x=offset.x+81, y=offset.y+19, z=offset.z+81},
-							{x=offset.x+82, y=offset.y+19, z=offset.z+82},
-							{x=offset.x+80, y=offset.y+20, z=offset.z+80},
-							{x=offset.x+81, y=offset.y+20, z=offset.z+81},
-							{x=offset.x+82, y=offset.y+20, z=offset.z+82},
-							{x=offset.x+81, y=offset.y+21, z=offset.z+81},
-						}
+local barriers = {}
 
-		function set_barriers(node)
-			for _, i in pairs(barriers) do
-				minetest.set_node(i, {name=node})
-			end
-		end
+minetest.register_on_mods_loaded(function()
+	local map = ctf_modebase.map_catalog.maps[ctf_modebase.map_catalog.map_dirnames.ant_war]
 
-        set_barriers("ctf_map:obsidian")
+	barriers = {
+		map.offset:offset(80, 13, 82),
+		map.offset:offset(80, 14, 82),
+		map.offset:offset(81, 13, 81),
+		map.offset:offset(81, 14, 81),
+		map.offset:offset(81, 15, 81),
+		map.offset:offset(82, 13, 80),
+		map.offset:offset(82, 13, 80),
+		map.offset:offset(82, 14, 80),
 
-        local function remove_barriers()
-			local map = ctf_modebase.map_catalog.maps[ctf_modebase.map_catalog.current_map].name
-				if map == "Ant War" and closed and math.random(5) == 1 then
-					minetest.after(math.random(6, 10), function()
-						set_barriers("default:obsidian") minetest.chat_send_all(minetest.colorize("#f49200", "The central passages have been opened!"))
-						closed = false
-					end)
-				end
-		end
-
-        local function cleanup()
-			for i, f in pairs(ctf_api.registered_on_flag_take) do
-				if f == remove_barriers then
-					ctf_api.registered_on_flag_take[i] = nil
-					break
-				end
-			end
-			for i, f in pairs(ctf_api.registered_on_match_end) do
-				if f == cleanup then
-					ctf_api.registered_on_match_end[i] = nil
-					break
-				end
-			end
-		end
-
-        ctf_api.register_on_flag_take(remove_barriers)
-		ctf_api.register_on_match_end(cleanup)
-    end
+		map.offset:offset(80, 19, 80),
+		map.offset:offset(80, 20, 80),
+		map.offset:offset(81, 19, 81),
+		map.offset:offset(81, 20, 81),
+		map.offset:offset(81, 21, 81),
+		map.offset:offset(82, 19, 82),
+		map.offset:offset(82, 20, 82),
+	}
 end)
 
+local function set_barriers(node)
+	for _, i in pairs(barriers) do
+		minetest.set_node(i, {name=node})
+	end
+end
 
+ctf_api.register_on_new_match(function()
+	if ctf_map.current_map.dirname == "ant_war" then
+		closed = true
+
+		set_barriers("ctf_map:obsidian")
+	end
+end)
+
+ctf_api.register_on_flag_take(function()
+	if ctf_map.current_map.dirname == "ant_war" and closed and math.random(5) == 1 then
+		closed = false
+		minetest.after(math.random(6, 10), function()
+			set_barriers("default:obsidian")
+			minetest.chat_send_all(minetest.colorize("#f49200", "The central passages have been weakened!"))
+		end)
+	end
+end)
