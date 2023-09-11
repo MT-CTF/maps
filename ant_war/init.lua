ea3d92b85edeaf1dbd66c4f3742731ec97846140
lazylier <80552599+lazylier@users.noreply.github.com>
2023-09-10 20:45:24 -0400

    fix init.lua and change barriers to lava

--- ant_war/init.lua
+++ ant_war/init.lua
@@ -1,29 +1,22 @@
 local closed = true
+local map = ctf_modebase.map_catalog.maps[ctf_modebase.map_catalog.map_dirnames.ant_war]
+local barriers = {
+	map.offset:offset(80, 13, 82),
+	map.offset:offset(80, 14, 82),
+	map.offset:offset(81, 13, 81),
+	map.offset:offset(81, 14, 81),
+	map.offset:offset(81, 15, 81),
+	map.offset:offset(82, 13, 80),
+	map.offset:offset(82, 14, 80),
 
-local barriers = {}
-
-minetest.register_on_mods_loaded(function()
-	local map = ctf_modebase.map_catalog.maps[ctf_modebase.map_catalog.map_dirnames.ant_war]
-
-	barriers = {
-		map.offset:offset(80, 13, 82),
-		map.offset:offset(80, 14, 82),
-		map.offset:offset(81, 13, 81),
-		map.offset:offset(81, 14, 81),
-		map.offset:offset(81, 15, 81),
-		map.offset:offset(82, 13, 80),
-		map.offset:offset(82, 13, 80),
-		map.offset:offset(82, 14, 80),
-
-		map.offset:offset(80, 19, 80),
-		map.offset:offset(80, 20, 80),
-		map.offset:offset(81, 19, 81),
-		map.offset:offset(81, 20, 81),
-		map.offset:offset(81, 21, 81),
-		map.offset:offset(82, 19, 82),
-		map.offset:offset(82, 20, 82),
-	}
-end)
+	map.offset:offset(80, 19, 80),
+	map.offset:offset(80, 20, 80),
+	map.offset:offset(81, 19, 81),
+	map.offset:offset(81, 20, 81),
+	map.offset:offset(81, 21, 81),
+	map.offset:offset(82, 19, 82),
+	map.offset:offset(82, 20, 82),
+}
 
 local function set_barriers(node)
 	for _, i in pairs(barriers) do
@@ -34,7 +27,6 @@ end
 ctf_api.register_on_new_match(function()
 	if ctf_map.current_map.dirname == "ant_war" then
 		closed = true
-
 		set_barriers("ctf_map:obsidian")
 	end
 end)
@@ -43,8 +35,10 @@ ctf_api.register_on_flag_take(function()
 	if ctf_map.current_map.dirname == "ant_war" and closed and math.random(5) == 1 then
 		closed = false
 		minetest.after(math.random(6, 10), function()
-			set_barriers("default:obsidian")
-			minetest.chat_send_all(minetest.colorize("#f49200", "The central passages have been weakened!"))
+			minetest.chat_send_all(minetest.colorize("#f49200", "The central barriers are about to melt!"))
+			minetest.after(math.random(6, 10), function()
+				set_barriers("default:lava_source")
+			end)
 		end)
 	end
 end)
